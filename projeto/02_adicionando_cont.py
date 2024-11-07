7edb7b12e4c804c9ea8a35aca2c99a117fdd2971
Diego Damasceno <136119323+diegodamascenos@users.noreply.github.com>
2024-11-07 18:17:10 -0300

    segundaparte

--- projeto/02_adicionando_cont.py
+++ projeto/02_adicionando_cont.py
@@ -3,11 +3,12 @@ from pathlib import Path
 import streamlit as st
 import pandas as pd
 
-pasta_superstore_sales = Path(__file__).parent / 'superstore_sales'
-df_train = pd.read_csv(pasta_superstore_sales / 'train.csv', decimal=',')
-df_products = pd.read_csv(pasta_superstore_sales / 'products.csv', decimal=',')
-df_customers = pd.read_csv(pasta_superstore_sales / 'customers.csv', decimal=',')
+pasta_superstore_sales = Path(__file__).parent.parent / 'superstore_sales'
+caminho_superstore = pasta_superstore_sales / 'train.csv'
+
+df_train = pd.read_csv('train.csv', decimal=',')
+df_train['City/State'] = df_train['City']
+lista_filiais = df_train['City/State'].tolist()
+st.sidebar.selectbox('Selecione a região:', lista_filiais)
 
 st.dataframe(df_train)
-st.dataframe (df_products)
-st.dataframe(df_customers)
